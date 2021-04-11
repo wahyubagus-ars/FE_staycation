8da561747d50cad750ded4adeac8f8ce3c0cc8e5
wahyubagus-ars <babayaga558@gmail.com>
2021-04-11 15:01:44 +0700

    checkout page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route} from 'react-router-dom'
 import LandingPage from 'pages/LandingPage';
 import DetailPage from 'pages/DetailPage';
 import Example from 'pages/Example';
+import Checkout from 'pages/Checkout';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
         <Route exact path="/properties/:id" component={DetailPage}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
       </Router>
     </div>
   );
--- src/elements/Form/index.js
+++ src/elements/Form/index.js
@@ -1,2 +1,4 @@
 export {default as InputNumber} from './inputNumber'
 export {default as InputDate} from './inputDate'
+export {default as InputText} from './InputText'
+export {default as InputFile} from './InputFile'
